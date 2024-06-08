88522d40ad0f4eeee4f2dbb1cb781d404e7e6796
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2024-06-08 14:02:52 +0530

    forward direction scroll working

--- src/app/static/virtual-own/List2.tsx
+++ src/app/static/virtual-own/List2.tsx
@@ -5,47 +5,62 @@ export const List = ({list, Item, containerHeight, itemHeight}) => {
     start: 0,
     end: Math.min(list.length - 1, Math.ceil(containerHeight / itemHeight)),
   });
-  const scrollRef = useRef(0);
-  const parentContainerRef = useRef(null);
+    const scrollRef = useRef(0);
+    const parentContainerRef = useRef(null);
 
-  const calcListToRender = () => {
-    //@ts-ignore
-    const scrollTop = parentContainerRef.current.parentElement.scrollTop;
-    const lengthScrolled = scrollTop - scrollRef.current;
-    const startIndexOffset = scrollTop;
-    const endIndexOffset = startIndexOffset + containerHeight;
-    const startIndex = Math.max(
-      0,
-      Math.floor(startIndexOffset / (itemHeight + 20))
-    ); // how much you have scrolled / item height will give start index
-    const endIndex = Math.ceil(endIndexOffset / (itemHeight - 20));
-    // const startIndex = Math.max(0, Math.floor(scrollTop / (itemHeight + 20))); // no of elements scrolled
-    // const endIndex = Math.min(
-    //   list.length,
-    //   Math.min(
-    //     list.length,
-    //     Math.ceil((scrollTop + containerHeight) / (itemHeight - 20))
-    //   )
-    // );
-    setListToRender({
-      start: startIndex,
-      end: endIndex,
-    });
-    scrollRef.current = scrollTop;
-    console.log("container height", containerHeight);
-    console.log("scroll top", scrollTop);
-    console.log("start", startIndex);
-    console.log("end", endIndex);
-  };
+    const calcListToRender = () => {
+      //@ts-ignore
+      const scrollTop = parentContainerRef.current.parentElement.scrollTop;
+      const lengthScrolled = scrollTop - scrollRef.current;
+      if (scrollTop === scrollRef.current) {
+        return;
+      }
+        if (lengthScrolled < itemHeight && scrollTop > scrollRef.current) {
+          console.log("LENTHG SCROLLED < ITEM HEIGHT");
+          setListToRender({
+            start: Math.max(0, listToRender.start + 1),
+            end: Math.min(list.length - 1, listToRender.end + 1),
+          });
+          return;
+        }
+        if (lengthScrolled < itemHeight && scrollTop < scrollRef.current) {
+          console.log(scrollRef.current, scrollTop);
+          console.log("LENTHG SCROLLED < ITEM HEIGHT, scrolling up");
+          setListToRender({
+            start: Math.max(0, listToRender.start - 1),
+            end: Math.min(list.length - 1, listToRender.end - 1),
+          });
+          return;
+        }
+      const startIndexOffset = scrollTop;
+      const endIndexOffset = startIndexOffset + containerHeight;
+      const startIndex = Math.max(0, Math.floor(startIndexOffset / itemHeight)); // how much you have scrolled / item height will give start index
+      const endIndex = Math.ceil(endIndexOffset / itemHeight);
+      // const startIndex = Math.max(0, Math.floor(scrollTop / (itemHeight + 20))); // no of elements scrolled
+      // const endIndex = Math.min(
+      //   list.length,
+      //   Math.min(
+      //     list.length,
+      //     Math.ceil((scrollTop + containerHeight) / (itemHeight - 20))
+      //   )
+      // );
+      setListToRender({
+        start: startIndex,
+        end: endIndex,
+      });
+      scrollRef.current = scrollTop;
+      console.log("container height", containerHeight);
+      console.log("scroll top", scrollTop);
+      console.log("start", startIndex);
+      console.log("end", endIndex);
+    };
 
-  return (
-    <div ref={parentContainerRef} onWheel={calcListToRender}>
-      <div>asdf</div>
-      {/*@ts-ignore*/}
-      {list.slice(listToRender.start, listToRender.end).map((item) => (
-        <Item key={item}>{item}</Item>
-      ))}
-      <div>asdf</div>
-    </div>
-  );
+    return (
+      <div ref={parentContainerRef} onWheel={calcListToRender}>
+        {/*@ts-ignore*/}
+        {list.slice(listToRender.start, listToRender.end).map((item) => (
+          <Item key={item}>{item}</Item>
+        ))}
+      </div>
+    );
 };
