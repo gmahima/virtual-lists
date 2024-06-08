8aae7d1ab9871d8bc0ee52d6a524f85ba1508756
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2024-06-08 17:29:12 +0530

    scroll bottom works

--- src/app/static/virtual-own/List2.tsx
+++ src/app/static/virtual-own/List2.tsx
@@ -3,57 +3,41 @@ import React, {useEffect, useRef, useState} from "react";
 export const List = ({list, Item, containerHeight, itemHeight}) => {
   const [listToRender, setListToRender] = useState({
     start: 0,
-    end: Math.min(list.length - 1, Math.ceil(containerHeight / itemHeight)),
+    end: Math.min(list.length - 1, Math.ceil(containerHeight / itemHeight) + 2),
   });
-    const scrollRef = useRef(0);
-    const parentContainerRef = useRef(null);
+  const scrollRef = useRef(0);
+  const parentContainerRef = useRef(null);
 
-    const calcListToRender = () => {
-      //@ts-ignore
-      const scrollTop = parentContainerRef.current.parentElement.scrollTop;
-      const lengthScrolled = scrollTop - scrollRef.current;
-      if (scrollTop === scrollRef.current) {
-        return;
-      }
-        if (lengthScrolled < itemHeight && scrollTop > scrollRef.current) {
-          console.log("LENTHG SCROLLED < ITEM HEIGHT");
-          setListToRender({
-            start: Math.max(0, listToRender.start + 1),
-            end: Math.min(list.length - 1, listToRender.end + 1),
-          });
-          return;
-        }
-        if (lengthScrolled < itemHeight && scrollTop < scrollRef.current) {
-          console.log(scrollRef.current, scrollTop);
-          console.log("LENTHG SCROLLED < ITEM HEIGHT, scrolling up");
-          setListToRender({
-            start: Math.max(0, listToRender.start - 1),
-            end: Math.min(list.length - 1, listToRender.end - 1),
-          });
-          return;
-        }
-      const startIndexOffset = scrollTop;
-      const endIndexOffset = startIndexOffset + containerHeight;
-      const startIndex = Math.max(0, Math.floor(startIndexOffset / itemHeight)); // how much you have scrolled / item height will give start index
-      const endIndex = Math.ceil(endIndexOffset / itemHeight);
-      // const startIndex = Math.max(0, Math.floor(scrollTop / (itemHeight + 20))); // no of elements scrolled
-      // const endIndex = Math.min(
-      //   list.length,
-      //   Math.min(
-      //     list.length,
-      //     Math.ceil((scrollTop + containerHeight) / (itemHeight - 20))
-      //   )
-      // );
-      setListToRender({
-        start: startIndex,
-        end: endIndex,
-      });
-      scrollRef.current = scrollTop;
-      console.log("container height", containerHeight);
-      console.log("scroll top", scrollTop);
-      console.log("start", startIndex);
-      console.log("end", endIndex);
-    };
+  const calcListToRender = () => {
+    //@ts-ignore
+    const scrollTop = parentContainerRef.current.parentElement.scrollTop;
+    const lengthScrolled = scrollTop - scrollRef.current;
+    console.log("length scrolled", lengthScrolled);
+    if (scrollTop === scrollRef.current) {
+      return;
+    }
+    const startIndexOffset = scrollTop - 80;
+    const endIndexOffset = scrollTop + containerHeight + 80;
+    const startIndex = Math.max(0, Math.floor(startIndexOffset / itemHeight)); // how much you have scrolled / item height will give start index
+    const endIndex = list.length;
+    // const startIndex = Math.max(0, Math.floor(scrollTop / (itemHeight + 20))); // no of elements scrolled
+    // const endIndex = Math.min(
+    //   list.length,
+    //   Math.min(
+    //     list.length,
+    //     Math.ceil((scrollTop + containerHeight) / (itemHeight - 20))
+    //   )
+    // );
+    setListToRender({
+      start: startIndex,
+      end: endIndex,
+    });
+    scrollRef.current = scrollTop;
+    console.log("container height", containerHeight);
+    console.log("scroll top", scrollTop);
+    console.log("start", startIndex);
+    console.log("end", endIndex);
+  };
 
     return (
       <div ref={parentContainerRef} onWheel={calcListToRender}>
