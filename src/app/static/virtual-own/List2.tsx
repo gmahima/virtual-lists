57a6dcaaf7933ed6f272fe71d7567f29d6df218b
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2024-06-08 17:34:18 +0530

    both work

--- src/app/static/virtual-own/List2.tsx
+++ src/app/static/virtual-own/List2.tsx
@@ -16,10 +16,16 @@ export const List = ({list, Item, containerHeight, itemHeight}) => {
     if (scrollTop === scrollRef.current) {
       return;
     }
-    const startIndexOffset = scrollTop - 80;
-    const endIndexOffset = scrollTop + containerHeight + 80;
-    const startIndex = Math.max(0, Math.floor(startIndexOffset / itemHeight)); // how much you have scrolled / item height will give start index
-    const endIndex = list.length;
+    const startIndexOffset = scrollTop;
+    const endIndexOffset = scrollTop + containerHeight;
+    const startIndex = Math.max(
+      0,
+      Math.floor(startIndexOffset / (itemHeight + 20))
+    ); // how much you have scrolled / item height will give start index
+    const endIndex = Math.min(
+      list.length,
+      Math.ceil((scrollTop + containerHeight) / (itemHeight - 20))
+    );
     // const startIndex = Math.max(0, Math.floor(scrollTop / (itemHeight + 20))); // no of elements scrolled
     // const endIndex = Math.min(
     //   list.length,
